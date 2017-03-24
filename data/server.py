63766a5c8b62de9a08515dc799fafc640e6bbeb1
pingfreud <felipe.c.argolo@gmail.com>
2017-03-24 23:00:08 +0000

    working on client-server - Data sent and stored to db

--- data/server.py
+++ data/server.py
@@ -1,14 +1,30 @@
 from twisted.internet.protocol import Protocol, Factory
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet import reactor
+from twisted.enterprise import adbapi
+from twistar.registry import Registry
+from twistar.dbobject import DBObject
+from twisted.internet.defer import setDebugging
 
+setDebugging(True)
+
+class Patient(DBObject):
+    pass
+
+def done(patient):
+    print ("Patient %s was added to the database" % patient.first_name)
+    reactor.stop()
+
+ 
 class NamePrinter(Protocol):
     def dataReceived(self,data):
+        Registry.DBPOOL = adbapi.ConnectionPool('sqlite3', "patients.sqlite", check_same_thread=False)
+        p = Patient(first_name = str(data), last_name = "Argolo", age = 24)
         print ("The name is %s" % data)
-        self.transport.loseConnection()
+        p.save().addCallback(done)
         
     def connectionMade(self):
-        self.transport.write("Patient data was saved\n")
+        self.transport.write("Welcome, client! Type the name\n")
 
 class NameFactory(Factory):
     def buildProtocol(self,addr):
